cabac16a25c5445888778646ce581b06328ab066
Ssss455t <111704834+Ssss455t@users.noreply.github.com>
2022-10-04 13:59:11 +0200

    Update E3zem Weksab Vodafone By Abdullah.py

--- E3zem Weksab Vodafone By Abdullah.py
+++ E3zem Weksab Vodafone By Abdullah.py
@@ -13,10 +13,10 @@ try:
 except:
 	os.system ('pip install requests && pip install bs4 && pip install thread6 && pip install random')
 sleep (1)
-print ("\033[1;093mhttps://bestcash2020.com/6xJAL6P")
+print ("\033[1;093mhttps://bestcash2020.com/zVI749xS")
 sleep (1)
 Pas=input ("\033[1;092m》Enter The Script Password  :  ")
-if Pas!="Abdullah3444":
+if Pas!="Abdullah4550":
     print ("\033[1;091mError Password ")
     exit()
 else:
